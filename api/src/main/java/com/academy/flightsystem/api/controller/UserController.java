cffe3074620b85e658f8b122b0d16817be2642c6
svetoslavss <svetoslavd.stoyanovs@gmail.com>
2025-02-10 19:55:31 +0200

    UserController updated

--- api/src/main/java/com/academy/flightsystem/api/controller/UserController.java
+++ api/src/main/java/com/academy/flightsystem/api/controller/UserController.java
@@ -1,24 +1,43 @@
 package com.academy.flightsystem.api.controller;
 
+import com.academy.flightsystem.api.exceptions.BadRequestException;
+import com.academy.flightsystem.api.exceptions.ProfileNotFoundException;
 import com.academy.flightsystem.api.model.UserInfo;
+import com.academy.flightsystem.api.response.ApiResponse;
 import com.academy.flightsystem.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/users")
 public class UserController {
-    // Authentication manager
-    // jwtUtil
-    // repo + service
 
     @Autowired
     private UserService service;
 
     @PostMapping("/register")
-    public UserInfo register(@RequestBody UserInfo userInfo){
-        return service.register(userInfo);
+    public ResponseEntity<ApiResponse> register(@RequestBody UserInfo user){
+        try {
+            UserInfo registeredUser = service.register(user);
+            return ResponseEntity.ok(new ApiResponse("Successfully registered", registeredUser));
+        } catch (BadRequestException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
     }
 
+    @GetMapping("/profile")
+    public ResponseEntity<ApiResponse> getProfile(){
+       try {
+           UserInfo userProfile = service.findProfile();
+           return ResponseEntity.ok(new ApiResponse("Successfully found profile", userProfile));
+       } catch (ProfileNotFoundException e) {
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+       }
+    }
 }
