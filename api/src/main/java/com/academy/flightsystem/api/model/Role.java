8cdcb896733995e8cac052c3b3b5e34feba7b0e6
svetoslavss <svetoslavd.stoyanovs@gmail.com>
2025-02-10 19:52:59 +0200

    Role entity created

--- api/src/main/java/com/academy/flightsystem/api/model/Role.java
+++ api/src/main/java/com/academy/flightsystem/api/model/Role.java
@@ -1,2 +1,30 @@
-package com.academy.flightsystem.api.model;public class Role {
+package com.academy.flightsystem.api.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToMany
+    private Set<UserInfo> users = new HashSet<>();
+
+    public Role(String name){
+        this.name = name;
+    }
+
 }
