0b2cf2d96d75355a098e6ac979f33fbd90c74b78
svetoslavss <svetoslavd.stoyanovs@gmail.com>
2025-02-10 19:52:03 +0200

    Data initializer created

--- api/src/main/java/com/academy/flightsystem/api/utils/DataInitializer.java
+++ api/src/main/java/com/academy/flightsystem/api/utils/DataInitializer.java
@@ -12,14 +12,14 @@ public class DataInitializer implements CommandLineRunner {
     @Autowired
     private RoleRepository roleRepository;
 
+
     @Override
     public void run(String... args) throws Exception {
         if (roleRepository.findByName("ROLE_USER").isEmpty()){
-            roleRepository.save(new Role("ROLE_USER"));
-        }
-
-        if (roleRepository.findByName("ROLE_ADMIN").isEmpty()){
-            roleRepository.save(new Role("ROLE_ADMIN"));
-        }
+            roleRepository.save((new Role("ROLE_USER")));
     }
+        if(roleRepository.findByName("ROLE_ADMIN").isEmpty()){
+            roleRepository.save((new Role("ROLE_ADMIN")));
+        }
+}
 }
