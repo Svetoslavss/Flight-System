582ab02ab18940ca5cd9d25d31e561c3fe0f877e
svetoslavss <svetoslavd.stoyanovs@gmail.com>
2025-02-10 19:54:21 +0200

    Map the Role entity for database

--- api/src/main/java/com/academy/flightsystem/api/model/UserInfo.java
+++ api/src/main/java/com/academy/flightsystem/api/model/UserInfo.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 
 @Entity
 @Data
@@ -18,6 +21,10 @@ public class UserInfo {
     private Long id;
     private String username;
     private String password;
-    private String roles = "USER";
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 }
